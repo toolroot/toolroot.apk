6ea1e5c43c4124d41b91e975cce2bd2319084851
mwear3robloxandroid <mitchellwears@gmail.com>
2017-03-04 15:29:15 -0500

    Create root.java

---
+++ root.java
@@ -0,0 +1,8 @@
+allow access
+<canvas id="myCanvas" width="70" height="70"></canvas>
+<button type="button">allow</button>
+get.device.root();
+get.device.superuser("allow");
+<button type="button">denied</button>
+get.device.root();
+get.device.superuser("denied");
